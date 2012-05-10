2fd580c9092fa840054c505c23ea76bf73f6dbfa
ruslan (home) <ruslanchek@me.com>
2012-05-10 21:36:50 +0400

    ddd

--- control/templates_c/46f902f1a7924c840ca8edd7672aeaad6d50655b.file.footer.tpl.php
+++ control/templates_c/46f902f1a7924c840ca8edd7672aeaad6d50655b.file.footer.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.7, created on 2012-04-07 13:35:36
+<?php /* Smarty version Smarty-3.1.7, created on 2012-05-10 21:35:59
          compiled from "/Users/ruslan/Documents/sites/gps/control/templates/common/footer.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:11611021034f800a68ec2912-21368212%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '46f902f1a7924c840ca8edd7672aeaad6d50655b' => 
     array (
       0 => '/Users/ruslan/Documents/sites/gps/control/templates/common/footer.tpl',
-      1 => 1333734584,
+      1 => 1336671326,
       2 => 'file',
     ),
   ),
@@ -15,8 +15,8 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
-  'has_nocache_code' => false,
   'version' => 'Smarty-3.1.7',
   'unifunc' => 'content_4f800a68ec387',
+  'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_4f800a68ec387')) {function content_4f800a68ec387($_smarty_tpl) {?><?php }} ?>
--- control/templates_c/820f25f63b8cdf8afafd084f38a2bf0c57742ce0.file.main.tpl.php
+++ control/templates_c/820f25f63b8cdf8afafd084f38a2bf0c57742ce0.file.main.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.7, created on 2012-05-10 01:21:46
+<?php /* Smarty version Smarty-3.1.7, created on 2012-05-10 21:35:59
          compiled from "/Users/ruslan/Documents/sites/gps/control/templates/main.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:11291423434f800a68d221c3-26876524%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '820f25f63b8cdf8afafd084f38a2bf0c57742ce0' => 
     array (
       0 => '/Users/ruslan/Documents/sites/gps/control/templates/main.tpl',
-      1 => 1336598505,
+      1 => 1336671326,
       2 => 'file',
     ),
   ),
@@ -39,26 +39,46 @@ $_valid = $_smarty_tpl->decodeProperties(array (
                         <span class="icon-bar"></span>
                     </a>
 
-                    <span class="brand">Автоконтроль</span>
+                    <?php if ($_smarty_tpl->tpl_vars['core']->value->module['name']=='main'){?>
+                        <span class="brand">Автоконтроль</span>
+                    <?php }else{ ?>
+                        <a class="brand" href="/control">Автоконтроль</a>
+                    <?php }?>
 
                     <div class="nav-collapse">
                         <ul class="nav">
-                            <li class="active"><a href="/control/map/">GPS-мониторинг</a></li>
-                            <li><a href="/control/system/">Бортовой компьютер</a></li>
-                            <li><a href="/control/guestbook/">Настройка</a></li>
-                            <li class="divider-vertical"></li>
-                            <li><a href="/control/help/"><i class="icon-question-sign icon-white"></i> Помощь</a></li>
+                            <?php if ($_smarty_tpl->tpl_vars['core']->value->auth->user_status['status']){?>
+                                <li<?php if ($_smarty_tpl->tpl_vars['core']->value->module['name']=='map'){?> class="active"<?php }?>><a href="/control/map/">GPS-мониторинг</a></li>
+                                <li<?php if ($_smarty_tpl->tpl_vars['core']->value->module['name']=='system'){?> class="active"<?php }?>><a href="/control/system/">Бортовой компьютер</a></li>
+                                <li class="divider-vertical"></li>
+                                <li><a href="/control/help/"><i class="icon-question-sign icon-white"></i> Помощь</a></li>
+                            <?php }?>
                         </ul>
                         <ul class="nav pull-right">
+                            <?php if ($_smarty_tpl->tpl_vars['core']->value->auth->user_status['status']){?>
+                                <li><a href="/control/help/"><span class="badge badge-warning">4</span> Уведомления</a></li>
+                            <?php }?>
+
                             <li class="divider-vertical"></li>
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user icon-white"></i> Авторизация <b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="/control/auth/login"><i class="icon-share-alt"></i> Вход</a></li>
-                                    <li><a href="/control/auth/register"><i class="icon-file"></i> Регистрация</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="/control/auth/remember_pass"><i class="icon-question-sign"></i> Напомнить пароль</a></li>
-                                </ul>
+                                <?php if ($_smarty_tpl->tpl_vars['core']->value->auth->user_status['status']){?>
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user icon-white"></i> <?php echo $_smarty_tpl->tpl_vars['core']->value->auth->user_status['userdata']['login'];?>
+ <b class="caret"></b></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="/control/user"><i class="icon-wrench"></i> Настройка</a></li>
+                                        <li><a href="/control/user/change_password"><i class="icon-pencil"></i> Сменить пароль</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="javascript:void(0)" onclick="core.exitUser()"><i class="icon-share"></i> Выйти</a></li>
+                                    </ul>
+                                <?php }else{ ?>
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user icon-white"></i> Авторизация <b class="caret"></b></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="/control/auth/login"><i class="icon-share-alt"></i> Вход</a></li>
+                                        <li><a href="/control/auth/register"><i class="icon-file"></i> Регистрация</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="/control/auth/remember_pass"><i class="icon-question-sign"></i> Напомнить пароль</a></li>
+                                    </ul>
+                                <?php }?>
                             </li>
                         </ul>
                     </div>
@@ -69,6 +89,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
         <div class="container-fluid main_content">
             <?php echo $_smarty_tpl->getSubTemplate ("modules/".($_smarty_tpl->tpl_vars['core']->value->module['name']).".tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
 
+            <div class="clear"></div>
             <hr>
             <?php echo $_smarty_tpl->getSubTemplate ("common/footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
 
