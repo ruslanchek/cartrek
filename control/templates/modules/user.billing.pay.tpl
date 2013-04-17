ec0f74d5f229c4a6da10ceb9ba42c9fc16169291
Ruslan Shashkov <ruslanchek@gmail.com>
2013-04-17 20:44:35 +0400

    c

--- control/templates/modules/user.billing.pay.tpl
+++ control/templates/modules/user.billing.pay.tpl
@@ -1,5 +1,28 @@
 <div class="threequarter">
+    <form id="pay-form" class="forms" action="https://www.moneta.ru/assistant.htm" method="POST">
+        <h2>Оплата</h2>
 
+        <div class="form_message"></div>
+        <div class="form-item">
+            <label for="old_password" class="bold">Старый пароль <span class="error"></span></label>
+            <input
+                    class="text width-100"
+                    type="text"
+                    name="MNT_AMOUNT"
+                    id="MNT_AMOUNT"
+                    value="1.00"
+                    />
+        </div>
+
+        <input type="submit" name="send" class="btn blue" value="Оплатить"/>
+
+        <input type="hidden" name="MNT_ID" value="41534771"/>
+        <input type="hidden" name="MNT_TRANSACTION_ID" value="1"/>
+        <input type="hidden" name="MNT_CURRENCY_CODE" value="RUR"/>
+        <input type="hidden" name="MNT_TEST_MODE" value="1"/>
+        <input type="hidden" name="MNT_DESCRIPTION" value="Test payment"/>
+
+    </form>
 </div>
 
 <div class="quarter">
--- control/user/billing/pay/Pay.class.php
+++ control/user/billing/pay/Pay.class.php
@@ -1,67 +1,35 @@
 <?php
 
-Class Pay extends Core {
-    public function __construct(){
+Class Pay extends Core
+{
+    private $account_id = 41534771,
+        $login = 'ruslan@fortyfour.ru',
+        $password = 'Loopusinfabula',
+        $payment_password = '71462';
+
+
+    public function __construct()
+    {
         parent::__construct();
 
-        if(!$this->auth->user['status']){
+        if (!$this->auth->user['status']) {
             header('Location: /control/auth/login');
-        };
+        }
+        ;
 
         $this->template = 'user.tpl';
 
         $this->init(array(
-            'name'  => 'user.billing.pay',
+            'name' => 'user.billing.pay',
             'title' => 'Оплата',
-            'dir'   => '/control/user/billing/pay'
+            'dir' => '/control/user/billing/pay'
         ));
     }
 
-    public function __destruct(){
+    public function __destruct()
+    {
         $this->deInit();
     }
 
-    private function monetaRu(){
-        require_once 'monetaru/MonetaWebService.php';
 
-        // Здесь указан тестовый URL (рабочий https://www.moneta.ru/services.wsdl)
-        // Также нужно указать логин и пароль
-        $service = new MonetaWebService("https://demo.moneta.ru/services.wsdl", "test@moneta.ru", "password");
-
-        try {
-            // получить данные счета
-            $response = $service->FindAccountById(10999);
-
-            echo "Current balance:\n";
-            echo "balance: {$response->account->availableBalance}\n";
-            echo "currency: {$response->account->currency}\n\n";
-
-            // перевод
-
-            $mtr = new MonetaTransferRequest(); //MonetaTransactionRequestType();
-            $mtr->amount = 10;
-            $mtr->payee = 12345678;
-            $mtr->payer = 10999;
-            $mtr->paymentPassword = "12345";
-
-            $trt = $service->Transfer($mtr);
-
-            // данные транзакции
-            echo "Transfer result:\n";
-            echo "status: {$trt->status}\n";
-            echo "date: {$trt->dateTime}\n";
-            echo "trxId: {$trt->transaction}\n\n";
-
-
-            // проверить данные счета
-            $response = $service->FindAccountById(10999);
-            echo "Balance after transfer:\n";
-            echo "balance: {$response->account->availableBalance}\n";
-            echo "currency: {$response->account->currency}\n";
-
-        } catch (Exception $e){
-            echo $e->getMessage();
-            echo $e->getTraceAsString();
-        };
-    }
-};
+}
